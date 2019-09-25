3a75f1baa47f6516807ccee79b213a60704037d6
taishikato <taishi.k0903@gmail.com>
2019-09-25 15:59:44 -0700

    :zap: save it

--- functions/index.js
+++ functions/index.js
@@ -5,22 +5,32 @@ const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
 const admin = require('firebase-admin')
-const cors = require('koa2-cors');
+const cors = require('@koa/cors');
+const bodyParser = require('koa-bodyparser');
 
 app.use(cors({
   origin: '*',
-  allowMethods: ['POST']
+  allowMethods: ['POST'],
+  allowHeaders: '*',
+  exposeHeaders: '*'
 }))
+app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 admin.initializeApp(functions.config().firebase)
 
 router.post('/getImageUrl', async (ctx) => {
+  console.log('request body')
+  console.log(ctx.request.body)
+  // TODO: Upload a image to firebase storage
+  // TODO: Get a download URL of the image
+  // TODO: return it
   ctx.response.status = 200
   ctx.body = {
     message: 'takato sensei'
   }
+  return
 })
 
 exports.apifunc = functions.https.onRequest(koaFirebase(app))
