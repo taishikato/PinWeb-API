05691d1ebb95bb46305bd87920cbf80e14be1bb4
taishikato <taishi.k0903@gmail.com>
2019-09-25 18:38:12 -0700

    :zap: Upload image and get download url

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const uuid = require('uuid/v4')
 const app = express()
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
@@ -13,10 +14,33 @@ app.post('/getImageUrl', (req, res) => {
   console.log('request body')
   console.log(req.body)
   // TODO: Upload a image to firebase storage
+  const bucket = admin.storage().bucket()
+  // Cut the first 23 strings(data:image/jpeg;base64,)
+  const base64Data = req.body.image.slice(22)
+  const imageBuffer = Buffer.from(base64Data, 'base64')
+  const imageByteArray = new Uint8Array(imageBuffer);
+  const fileName = uuid()
+    .split('-')
+    .join('')
+  const file = bucket.file(`images/${fileName}.jpeg`);
   // TODO: Get a download URL of the image
-  // TODO: return it
-  return res.json({ message: 'takato sensei' })
+  (async () => {
+    try {
+      await file.save(imageByteArray, { metadata: { contentType: 'image/jpeg', } })
+      const urls = await file.getSignedUrl({
+        action: 'read',
+        expires: '03-09-2500'
+      })
+      const url = urls[0]
+      console.log(`Image url = ${url}`)
+      // TODO: return it
+      return res.json({
+        image: url
+      })
+    } catch (err) {
+      console.log(`Error occured: ${err}`)
+    }
+  })();
 })
 
-// exports.apifunc = functions.https.onRequest(koaFirebase(app))
 exports.apifunc = functions.https.onRequest(app)
